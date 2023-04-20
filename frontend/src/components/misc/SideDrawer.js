32aa632abaaf191b5635228aa2093babd66d3f94
Jandueza <jonathanandueza1201@gmail.com>
2023-04-20 16:37:39 -0300

    SideDrawer & ProfileModal created

--- frontend/src/components/misc/SideDrawer.js
+++ frontend/src/components/misc/SideDrawer.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
-import { Menu, MenuButton, Tooltip } from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, Tooltip } from '@chakra-ui/react';
 import { Box, Text } from '@chakra-ui/layout';
 import { Button } from '@chakra-ui/button';
 import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Avatar } from "@chakra-ui/avatar";
+import { ChatState } from '../../Context/ChatProvider';
+import { MenuDivider, MenuItem } from '@chakra-ui/menu';
+import ProfileModal from './ProfileModal';
 
 export default function SideDrawer() {
     const [search, setSearch] = useState("");
@@ -10,16 +14,17 @@ export default function SideDrawer() {
     const [loading, setLoading] = useState(false);
     const [loadingChat, setLoadingChat] = useState();
 
+    const { user } = ChatState();
+
     return (
         <>
             <Box
-                d="flex"
+                display={{ base: "none", md: "flex" }}
                 justifyContent="space-between"
                 alignItems="center"
-                bg="white"
+                bg="teal.400"
                 w="100%"
                 p="5px 10px 5px 10px"
-                borderWidth="5px"
             >
                 <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
                     <Button variant="ghost" >
@@ -37,6 +42,27 @@ export default function SideDrawer() {
                         <MenuButton p={1}>
                             <BellIcon fontSize="2xl" m={1} />
                         </MenuButton>
+                        {/* <MenuList>  </MenuList> */}
+                    </Menu>
+                    <Menu>
+                        <MenuButton 
+                            as={Button}
+                            rightIcon={<ChevronDownIcon />}
+                        >
+                            <Avatar 
+                                size="sm" 
+                                cursor="pointer"
+                                name={user.name}
+                                src={user.picture}
+                            />
+                        </MenuButton>
+                        <MenuList>
+                            <ProfileModal>
+                                {/* <MenuItem>My Profile</MenuItem> */}
+                            </ProfileModal>
+                            <MenuDivider />
+                            <MenuItem>Logout</MenuItem>
+                        </MenuList>
                     </Menu>
                 </div>
             </Box>
